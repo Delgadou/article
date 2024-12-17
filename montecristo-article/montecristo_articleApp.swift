4ef4582ccf736413dcf41789fc2f049f4e282a04
Henrique Delgado <delgado.ohenrique@gmail.com>
2024-12-17 08:44:01 -0300

    first commit

---
+++ montecristo-article/montecristo_articleApp.swift
@@ -0,0 +1,17 @@
+//
+//  montecristo_articleApp.swift
+//  montecristo-article
+//
+//  Created by Henrique Delgado on 17/12/24.
+//
+
+import SwiftUI
+
+@main
+struct montecristo_articleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
